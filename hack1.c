7da1814ba620edd47eee00ffa52305ab7eae2498
William <42358772+wmorehead3@users.noreply.github.com>
2018-08-23 16:21:12 -0500

    labs

---
+++ hack1.c
@@ -0,0 +1,17 @@
+/**
+ * Author: William Morehead
+ * Collaborator: Matt Kretsch
+ *Date: 08/23/2018
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("My name is William Morehead, I am majoring in software engineering.\n");
+
+  return 0;
+}
